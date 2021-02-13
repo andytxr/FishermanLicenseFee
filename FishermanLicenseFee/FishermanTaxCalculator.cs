78693086c6c56b0046ff2cac8eebd070c45c2790
andytxr <62211442+andytxr@users.noreply.github.com>
2021-02-13 02:19:57 -0300

    Add files via upload

---
+++ FishermanLicenseFee/FishermanTaxCalculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseFee
+{
+    class FishermanTaxCalculator
+    {
+
+        private char _category;
+        private int _age;
+        private string _name;
+
+        public char category
+        {
+            get
+            {
+                return _category;
+            }
+            set
+            {
+                _category = value;
+            }
+        }
+
+        public int age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                _age = value;
+            }
+        }
+
+        public string name
+        {
+            get
+            {
+                return _name;           
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public double anualTax
+        {
+            get
+            {
+                if(category=='C' && age>0)
+                {
+                    return 15;
+                }
+                else if (category == 'E' && age > 10)
+                {
+                    return 30;
+                }
+                else
+                {
+                    return 6;
+                }
+            }
+        }
+
+        public FishermanTaxCalculator()
+        {
+            name = "Default";
+            age = 0;
+            category = 'D';
+        }
+
+        public FishermanTaxCalculator(string n, int a, char c)
+        {
+            name = n;
+            age = a;
+            category = c;
+        }
+    }
+}
---
+++ FishermanLicenseFee/LicenseFee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LicenseFee
+{
+    class AmbientalLicenseFee
+    {
+        static void Main(string[] args)
+        {
+
+            FishermanTaxCalculator f = new FishermanTaxCalculator();
+            int qtd;
+
+            Console.WriteLine("Number of fishermen: ");
+            qtd = int.Parse(Console.ReadLine());
+
+            if(qtd<1)
+            {
+                Console.WriteLine("The quantity needs to be a positive number!");
+                Console.WriteLine("Number of fishermen: ");
+                qtd = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i<qtd;i++)
+            {
+                Console.WriteLine("Name: ");
+                f.name = Console.ReadLine();
+
+                Console.WriteLine("Category: ");
+                f.category = char.Parse(Console.ReadLine());
+                f.category = Char.ToUpper(f.category);
+
+                if(f.category!='C'&&f.category!='E')
+                {
+                    do
+                    {
+                        Console.WriteLine("Error! Enter with Category 'C' or 'E' (C to coastal fishing and E to embarked fishing)");
+                        Console.WriteLine("Category: ");
+                        f.category = char.Parse(Console.ReadLine());
+                        f.category = Char.ToUpper(f.category);
+                    } while (f.category != 'C'&&f.category!='E');
+                }
+
+                Console.WriteLine("Age: ");
+                f.age = int.Parse(Console.ReadLine());
+
+                if(f.age<0)
+                {
+                    do
+                    {
+                        Console.WriteLine("Error! Age are only positive numbers.");
+                        Console.WriteLine("Age: ");
+                        f.age = int.Parse(Console.ReadLine());
+                    } while (f.age < 0);
+                }
+
+                Console.WriteLine("Anual Tax: {0:c}", f.anualTax);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
